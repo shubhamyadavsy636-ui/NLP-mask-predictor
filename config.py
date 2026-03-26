a3a18c0911ef3cdf6569bb565117ff7866ca2c6c
Shubham_yadav_tech <shubhamyadavsy636@gmail.com>
2026-03-26 10:04:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 MODEL_NAME = "mini-bert-fast"
 
-MAX_LENGTH = 64            # 🔥 biggest speed boost
-GRAD_ACCUM = 2            # reduce delay in updates
-BATCH_SIZE = 16           # 3050 can handle this at 64 tokens
+MAX_LENGTH = 64            
+GRAD_ACCUM = 2            
+BATCH_SIZE = 16           
 
-EPOCH = 3                 # enough for wikitext-2
-LR = 3e-5                 # slightly higher for faster convergence
+EPOCH = 3                 
+LR = 3e-5                 
 
-HIDDEN_SIZE = 256         # lighter model
-NUM_LAYERS = 6            # reduced from 8
-NUM_HEAD = 4              # must divide hidden size
+HIDDEN_SIZE = 256         
+NUM_LAYERS = 6            
+NUM_HEAD = 4              
 
 MLM_PROB = 0.15
 
-FP16 = True               # MUST for speed on 3050
-NUM_WORKERS = 6           # Ryzen 5 can handle more threads
+FP16 = True               
+NUM_WORKERS = 6           
 
 LOGS_STEPS = 200
 SAVE_STRATEGY = "epoch"
